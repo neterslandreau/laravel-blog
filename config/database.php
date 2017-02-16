1e65ec5643ce0c5b2af166469e73603312116b6f
Niles Rowland <neterslandreau@gmail.com>
2017-02-16 09:16:06 -0500

    change database name.

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'blog'),
+            'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
