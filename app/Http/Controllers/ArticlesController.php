5690858d248152dad5d78bd9205267804945ac40
Niles Rowland <neterslandreau@gmail.com>
2017-03-05 10:23:01 -0500

    open up tagged method in articles controller.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -11,7 +11,7 @@ class ArticlesController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except(['index', 'show']);
+        $this->middleware('auth')->except(['index', 'show', 'tagged']);
     }
 
     public function index()
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/articles/create', 'ArticlesController@create');
 Route::get('/articles/{slug}/edit', 'ArticlesController@edit');
 Route::get('/articles/{slug}/delete', 'ArticlesController@delete');
 Route::get('/articles/{slug}', 'ArticlesController@show');
-Route::post('articles/{slug}/edit', 'ArticlesController@edit');
+Route::post('/articles/{slug}/edit', 'ArticlesController@edit');
 Route::post('/articles', 'ArticlesController@store');
 Route::post('/articles/{slug}/comments', 'CommentsController@store');
 Route::get('/articles/tagged/{tag}', 'ArticlesController@tagged');
